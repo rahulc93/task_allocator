6327fd1ed389cf6dcbd078bde7e319a0abc3fd11
Rahul Chowdhury <rahulc93@gmail.com>
2015-05-16 22:28:09 +0530

    Add TimeZone option in form

--- mysite/task/forms.py
+++ mysite/task/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class TaskForm(forms.Form):
-    user_name = forms.CharField(label='User name', max_length=100)
+    task_type = forms.CharField( label = 'Task Type', max_length = 100 )
+    user_name = forms.CharField( label = 'User name', max_length = 100 )
+    country = forms.CharField( label = 'Country', max_length = 100 )
+    start_time = forms.DateTimeField( label = 'Start Time (Local)' )
+    end_time = forms.DateTimeField( label = 'End Time (Local)' )
+
--- mysite/task/views.py
+++ mysite/task/views.py
@@ -24,13 +24,48 @@ def get_task(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            user_name = form.cleaned_data['user_name']
-            print "User Name entered: ", user_name
+            un = form.cleaned_data['user_name']
+            print "User Name entered: ", un
+            c = form.cleaned_data['country']
+            print "Country entered: ", c
+            tt = form.cleaned_data['task_type']
+            print "Task type entered: ", tt
+            st = form.cleaned_data['start_time']
+            print "Start Time entered: ", st
+            print type(st)
+            et = form.cleaned_data['end_time']
+            print "End Time entered: ", et
+            print type(et)
+            request.session['django_timezone'] = request.POST['timezone']
+            print "Time Zone: ", request.session['django_timezone']
+            #time_zone = pytz.timezone( str( timezone.get_current_timezone_name() ) )
+            #time_zone = pytz.timezone('Asia/Kolkata')
+            st = st.replace(tzinfo=time_zone)
+            stu = st.astimezone(pytz.utc)
+            et = et.replace(tzinfo=time_zone)
+            etu = et.astimezone(pytz.utc)
+            curren_time = timezone.now()
+            #if curren_time >= st and curren_time <= et:
+                #print 'True'
+            #else:
+                #print 'False'
+
+            Configuration(task_type = tt, country = c, user_name = un, start_time = st, end_time = et, start_time_utc = stu, end_time_utc = etu, timezone = request.session['django_timezone']).save()
+            #form.save()
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            context = {'task_list': Configuration.objects.all(), 'timezones': pytz.common_timezones}
+            return render(request, 'task/task.html', context)
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = TaskForm()
 
     return render(request, 'task/task_entry.html', {'form': form})
+
+def set_timezone(request):
+    if request.method == 'POST':
+        request.session['django_timezone'] = request.POST['timezone']
+        print "django_timezone set to ", request.session['django_timezone']
+        return redirect('/task')
+    else:
+        return render(request, 'task/timezones.html', {'timezones': pytz.common_timezones})
